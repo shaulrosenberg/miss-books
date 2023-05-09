4f5da7c69e32af8365402bcd7e10e8b5d54c4084
shaulrosenberg <shaulr10@gmail.com>
2023-05-09 21:34:23 +0300

    Add files via upload

--- cmps/long-txt.jsx
+++ cmps/long-txt.jsx
@@ -1,20 +1,20 @@
-const {useState}  = React
+const { useState } = React
 
 
 export function LongTxt({ txt, length = 100 }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const toggleIsExpanded = () => setIsExpanded(!isExpanded);
-  
-    const truncatedTxt = txt.slice(0, length);
-    const displayTxt = isExpanded ? txt : truncatedTxt + '...';
-    const btnText = isExpanded ? 'Read less' : 'Read more';
-  
-    return (
-      <div>
-        <p>{displayTxt}</p>
-        {txt.length > length && (
-          <button onClick={toggleIsExpanded}>{btnText}</button>
-        )}
-      </div>
-    );
-  };
+	const [isExpanded, setIsExpanded] = useState(false)
+	const toggleIsExpanded = () => setIsExpanded(!isExpanded)
+
+	const truncatedTxt = txt.slice(0, length)
+	const displayTxt = isExpanded ? txt : truncatedTxt + '...'
+	const btnText = isExpanded ? 'Read less' : 'Read more'
+
+	return (
+		<div>
+			<p>{displayTxt}</p>
+			{txt.length > length && (
+				<button onClick={toggleIsExpanded}>{btnText}</button>
+			)}
+		</div>
+	)
+}
